abae1928e7d50fe547577feef3b668083eaef206
kos5667 <kos5667@gmail.com>
2024-03-05 22:42:03 +0900

    head: Mocha 자동화. body: Mocha 모델 실행.

--- test/app.js
+++ test/app.js
@@ -2,23 +2,21 @@
  * Test 자동화.
  *****************************************/
 const Mocha = require('mocha');
-const {runMocha} = require("mocha/lib/cli/run-helpers");
 
 // Mocha 옵션 객체 생성
-const mocha_options = {
+const options = {
 }
 
 // Mocha Test files.
 const mocha_files = [
-    './example/exampleTest.js'
+    './test/example/exampleTest.js'
 ]
 
+function runMocha () {
+    console.log('in...');
+    const mocha = new Mocha(options);
+    mocha.addFile('./test/example/exampleTest.js');
+    mocha.run()
+}
 
-runMocha(Mocha, mocha_options)
-    .then(r => {
-        console.log('in..')
-    })
-    .catch(e => {
-        console.log(e);
-    })
-
+runMocha();
