e2baff1fdbfef775068e5c937197f01b0d97af16
kos5667 <kos5667@gmail.com>
2024-03-08 00:45:29 +0900

    head: RoomModel.js body: create

---
+++ src/models/RoomModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const RoomSchema = new mongoose.Schema({
+    // 모델 스키마 정의
+    room_name: String,
+});
+
+module.exports = mongoose.model('Room', RoomSchema);
