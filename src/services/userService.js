81eecf2b04bc0747c20d40dfa7cc9e46f4e584c2
kos5667 <kos5667@gmail.com>
2024-03-08 00:01:06 +0900

    head: userService.js body: create userService.js

---
+++ src/services/userService.js
@@ -0,0 +1,13 @@
+module.exports = (UserModel) => {
+    const createUser = async (userData) => {
+        const user = new UserModel(userData);
+        return await user.save();
+    };
+
+    // 추가 서비스 메서드 정의
+
+    return {
+        createUser
+        // 기타 메서드 반환
+    };
+};
