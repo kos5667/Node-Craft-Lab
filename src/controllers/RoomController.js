0390d2dc0e82a384a5c59d9c56499b2c15f3055f
kos5667 <kos5667@gmail.com>
2024-03-08 00:45:21 +0900

    head: RoomController.js body: create

---
+++ src/controllers/RoomController.js
@@ -0,0 +1,11 @@
+module.exports = (RoomService) => {
+    const createRoom = async (req, res) => {
+    };
+
+    // 추가 컨트롤러 메서드 정의
+
+    return {
+        createRoom
+        // 기타 메서드 반환
+    };
+};
