6cb62999e7ddc55c4a5dfc34ef3cbe648ac2ee9f
kos5667 <kos5667@gmail.com>
2024-03-08 00:01:14 +0900

    head: userController.js body: create userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,17 @@
+module.exports = (UserService) => {
+    const createUser = async (req, res) => {
+        try {
+            const newUser = await UserService.createUser(req.body);
+            res.status(201).send(newUser);
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
+
+    // 추가 컨트롤러 메서드 정의
+
+    return {
+        createUser
+        // 기타 메서드 반환
+    };
+};
