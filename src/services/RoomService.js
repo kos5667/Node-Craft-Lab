e30eb6e7aeba4888ed51c68c1c6dad604ec7c92c
kos5667 <kos5667@gmail.com>
2024-03-09 01:31:45 +0900

    type: test body: 바인딩 테스트

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -1,19 +1,18 @@
-module.exports = (RoomModel) => {
-    const createRoom = async (roomData) => {
-        // const room = new RoomModel(roomData);
-        // return await room.save();
-        console.log('[RoomService] in......!!!!')
-        return 'ok!!'
-    };
+/**
+ * RoomService
+ */
+class RoomService {
 
-    const updateRoom = async (roomData) => {
-        console.log('updateRoom');
-    };
+    constructor() {
+        console.log('RoomService constructor!!')
+    }
 
-    // 추가 서비스 메서드 정의
-    return {
-        createRoom,
-        updateRoom,
-        // 기타 메서드 반환
-    };
-};
+    async createRoom(roomData) {
+        return 'RoomService ok!'
+    }
+
+    async updateRoom(roomData) {
+        return 'RoomService ok!'
+    }
+}
+module.exports = RoomService;
