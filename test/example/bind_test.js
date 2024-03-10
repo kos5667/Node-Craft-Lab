785a0ed2a04e6cfe732049618da510454095fd12
kos5667 <kos5667@gmail.com>
2024-03-10 22:52:58 +0900

    type: feat body: bind_test.js

---
+++ test/example/bind_test.js
@@ -0,0 +1,13 @@
+/**
+ * Binding Test.
+ */
+import('chai').then(mod => {
+
+    describe('### Binding Test 1', function() {
+
+    })
+
+    describe('### Binding Test 2', function() {
+
+    })
+});
