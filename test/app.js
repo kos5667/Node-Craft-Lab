b9fe61440b12a7c201ce90b5f031e6f9436a0c90
kos5667 <kos5667@gmail.com>
2024-03-11 08:50:58 +0900

    type: feat body: chai test

--- test/app.js
+++ test/app.js
@@ -4,29 +4,38 @@
 const Mocha = require('mocha')
     , async = require('async');
 
-// Mocha 옵션 객체 생성
+const DONT_REMOVE = './test/example/mocha_init.js';
 /**
- *
+ * Mocha 옵션
  * @type {Mocha.MochaOptions}
  */
 const options = {
-    // bail: true, // 첫 번째 테스트 실패 시 테스트 실행을 중단합니다.
-    // exit: true, // 테스트가 완료된 후 Mocha 프로세스를 강제로 종료합니다. 이 옵션은 비동기 작업이 완료되지 않았을 때 유용합니다.
-    // color: true,
+    bail: true, // 첫 번째 테스트 실패 시 테스트 실행을 중단합니다.
+    exit: true, // 테스트가 완료된 후 Mocha 프로세스를 강제로 종료합니다. 이 옵션은 비동기 작업이 완료되지 않았을 때 유용합니다.
+    color: true,
 }
 
-// Mocha Test files.
-const files = [
-    './test/example/mocha_test.js',
+/**
+ * Mocha Test files.
+ * @type {string[]}
+ */
+const mocha_files = [
+    // DONT_REMOVE,
+    /**
+     * chai 사용 시 최초 "import('chai')"가 생성되기 전
+     * (describe -> it)이 실행돼야 'chai'도 정상 실행됨.
+     * 삭제 금지. 'DONT_REMOVE'
+     */
+    // './test/example/mocha_test.js',
     // './test/example/chai_test.js',
     './test/example/bind_test.js',
 ]
 
 function runMocha () {
     const mocha = new Mocha(options);
-    files.forEach(fc => { mocha.addFile(fc); });
-    mocha.run((failures) => {
-        // console.log('failures: ',failures);
-    });
+
+    mocha_files.forEach(fc => { mocha.addFile(fc); });
+
+    mocha.run((failures) => { console.log('failures: ',failures); });
 }
 runMocha();
--- test/example/bind_test.js
+++ test/example/bind_test.js
@@ -1,14 +1,26 @@
+const axios = require('axios');
+const HOST = 'http://localhost:3000'
+const ROOM_HOST = `${HOST}/api/rooms/`
+const USER_HOST = `${HOST}/api/users/`
+
 /**
  * Binding Test.
  */
-const axios = require('axios');
 import('chai').then(mod => {
+    const should = mod.should();
+    const expect = mod.expect;
+    const assert = mod.assert;
 
-    describe('### Binding Test 1', function() {
-        require('../../src/controllers/RoomController')
-    })
-
-    describe('### Binding Test 2', function() {
-
+    describe('## Binding Test', function() {
+        describe('Room --->', function() {
+            it('Call API Room Fn => createRoom', function () {
+                // axios.get(`${ROOM_HOST}/createRoom`)
+                //     .then((r) => {
+                //         console.log('in...', r.data);
+                //         expect(r.data).to.null(null);
+                //         assert.isNull(r.data);
+                //     });
+            });
+        })
     })
 });
