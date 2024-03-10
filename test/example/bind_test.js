8ea4becbfbc5d85710d4672a47596a1f1559ee8c
kos5667 <kos5667@gmail.com>
2024-03-10 23:00:25 +0900

    type: feat body: axios install

--- test/example/bind_test.js
+++ test/example/bind_test.js
@@ -1,10 +1,11 @@
 /**
  * Binding Test.
  */
+const axios = require('axios');
 import('chai').then(mod => {
 
     describe('### Binding Test 1', function() {
-
+        require('../../src/controllers/RoomController')
     })
 
     describe('### Binding Test 2', function() {
