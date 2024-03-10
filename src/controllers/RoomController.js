c4029e33e16537228efdc7b8601ca4f5de6d8c99
kos5667 <kos5667@gmail.com>
2024-03-10 15:35:36 +0900

    type: test body: 바인딩 테스트

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,7 +4,6 @@ const {RoomServiceInstance} = require("../services");
  * RoomController
  */
 /** @type {RoomService} */
-let RoomService = null;
 class RoomController {
 
     constructor(RoomServiceInstance) {
@@ -12,11 +11,8 @@ class RoomController {
     }
 
     createRoom = async (req, res) => {
-        console.log('in... RoomController!!!')
         try {
-            console.log(this);
             const newRoom = await this.RoomService.createRoom('go!!');
-            // const newRoom = await RoomService.createRoom();
             res.status(201).send(newRoom);
         } catch (error) {
             console.error(error.message);
