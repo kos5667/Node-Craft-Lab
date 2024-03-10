97732c137fe1b974829b25d228ce99467b2f573f
kos5667 <kos5667@gmail.com>
2024-03-10 22:53:14 +0900

    type: feat body: mocha 테스트 자동화

--- test/app.js
+++ test/app.js
@@ -1,7 +1,8 @@
 /*****************************************
  * Test 자동화.
  *****************************************/
-const Mocha = require('mocha');
+const Mocha = require('mocha')
+    , async = require('async');
 
 // Mocha 옵션 객체 생성
 const options = {
@@ -9,14 +10,16 @@ const options = {
 
 // Mocha Test files.
 const files = [
-    './test/example/mocha_test.js',
-    './test/example/chai_test.js',
+    // './test/example/mocha_test.js',
+    // './test/example/chai_test.js',
+    './test/example/bind_test.js',
 ]
 
 function runMocha () {
-    console.log('in...');
     const mocha = new Mocha(options);
     files.forEach(fc => { mocha.addFile(fc); });
-    mocha.run();
+    mocha.run((failures) => {
+        console.log('failures: ',failures);
+    });
 }
 runMocha();
