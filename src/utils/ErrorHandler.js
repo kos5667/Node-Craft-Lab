d196bf84f91399e2734c4aeeca09994252b5b722
kos5667 <kos5667@gmail.com>
2024-03-17 10:20:13 +0900

    type: feat body: Create ErrorHandler.js

---
+++ src/utils/ErrorHandler.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+class ErrorHandler {
+    constructor() {
+    }
+}
+
+class ExampleErrorHandler extends ErrorHandler {
+    constructor() {
+        super();
+    }
+}
+
+module.exports = {
+    ErrorHandler,
+    ExampleErrorHandler
+}
