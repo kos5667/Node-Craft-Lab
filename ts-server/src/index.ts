b2ff30dd957c7ddceb47482a6277e146f084a7b2
kos5667 <kos5667@gmail.com>
2025-08-20 21:02:25 +0900

    feat: 프로젝트 초기화 body: n/a

--- ts-server/src/index.ts
+++ ts-server/src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello, TypeScript with Node.js!');
+import { Application } from "express";
+
+interface AppContext {
+    app: Application;
+}
+
+type StepFunction = (context: AppContext) => Promise<void>;
+
+const steps: StepFunction[] = [
+    async (context: AppContext) => {
+        console.log('Hello World!');
+    },
+];
+
+(async () => {
+    const context: AppContext = {
+        app: undefined as any
+    }
+
+    try {
+        for (const step of steps) {
+            await step(context);
+        }
+    } catch (err) {
+        console.error(err);
+        process.exit(0);
+    }
+})();
